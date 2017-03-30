3ea8fc191ee86d535c1e4f0f02ee30f7e799a25a
Unknown <kenne839@umn.edu>
2017-03-29 20:51:42 -0500

    Gyro and Step Counter Working Together
    SD card will save both to the same file depending on the mode.

--- src/step_counter_mpu9250/step_counter_mpu9250.ino
+++ src/step_counter_mpu9250/step_counter_mpu9250.ino
@@ -1,6 +1,19 @@
 /*********************
  * This appears to be working with arduino 1.8.1 IDE (latest?)
- 
+ ***********************
+ **
+ **********************************************************
+ **Made By: Michael Kennedy
+ **********************************************************
+ **
+ ** This is the main file for the Arduino Pro Mini to control the OpenLog and MPU-9250 modules
+ ** Could optimize the libraries to remove functions not used in the program if space is an issue.
+ ** 
+ ** This program relies on an outside source to set the UI_SET.TXT bit to the proper mode (step 1 or gyro 0)
+ ** Also needs outside source to clear SCNT.TXT b/c each power cycle will Append to it regardless if it's clear
+ **
+ ** Escape character is three of $$$ (ASCII 36)
+ **
  HAVE TO GO INTO ARDUINO LIBRARY FILES TO CHANGE SERIALPORT/SERIALPORT.H/ BUFFERED_TX AND ENABLE_RX_ERROR_CHECKING TO 0
  */
 
@@ -18,7 +31,7 @@
 #include "inv_mpu_dmp_motion_driver.h"
 #include "inv_mpu.h"
 #include "I2Cdev.h"
-#define DEFAULT_MPU_HZ  (20)
+#define DEFAULT_MPU_HZ  (200) //actually the max as well for DMP
  
 Timer t;
 unsigned long stepCount = 0;
@@ -27,16 +40,18 @@ unsigned long lastStepCount = 0;
 int ret;
 int error123;
 
-int UI_INT;
+int UI_INT; //variable to hold step count/ gyrometer setting
 int temp;
 
 int ledPin = 13;
 
 void setup() {
-	pinMode(ledPin, OUTPUT);
+	  
+	  pinMode(ledPin, OUTPUT);
     Fastwire::setup(400,0);
-    Serial.begin(57600);
-    delay(1000);
+    ret = mympu_open(200); //enable mpu
+    Serial.begin(57600);   //connect to OpenLog
+    delay(1000);           //Let the
 	
 	
 	Serial.println("reset");
@@ -80,18 +95,21 @@ void setup() {
 	  }
 	}
 	
-    
-    ret = mympu_open(200);
+  //If Step_count mode
+  //load the DMP module
+  //turn it on and initizlize step count/stepTime  
+	if(UI_INT == 1){
 		dmp_load_motion_driver_firmware();
 		dmp_set_fifo_rate(DEFAULT_MPU_HZ);
 		mpu_set_dmp_state(1);
 		
 		dmp_set_pedometer_walk_time(stepTime);
 		dmp_set_pedometer_step_count(stepCount);
+	}
 
     //int tickEvent = t.every(5000, ticker); //only will check status of steps every 10 seconds
 	
-
+  delay(10000); //test to see if gyrometer stabilizes after 10 seconds
     
 
 }
@@ -108,7 +126,11 @@ int state = 0;
 
 
 void loop() {
-    
+
+  //read DMP memory for step time and count
+  //update if it has changed
+  //might need to slow this down for less power?
+  //DMP hold the value anyway  
 	if(UI_INT == 1){ //if we want to count steps
     //t.update();
     dmp_get_pedometer_walk_time(&stepTime);
@@ -122,16 +144,17 @@ void loop() {
       //Serial.println(" (" + String((stepTime) / 1000.0) + " s)");
     }
 
-    digitalWrite(ledPin, (state) ? HIGH : LOW);
+    digitalWrite(ledPin, (state) ? HIGH : LOW); //toggle LED to view function
     state = !state;
 	}
 	
 	else if(!UI_INT){
-		ret = mympu_update(); //this was to update the gyro values, causes error when using DMP
+  
+		ret = mympu_update(); //update GRYO registers
 
 		switch (ret) {
 		case 0: c++; break;
-		case 1: np++; return;
+		case 1: np++; return;  //no packet error happens when running 8Mhz clock to a 200 hz update rate...
 		case 2: err_o++; return;
 		case 3: err_c++; return; 
 		default: 
@@ -140,6 +163,8 @@ void loop() {
 			return;
 		}	
 
+    
+  /*
 		if (!(c%25)) {
 			Serial.print(np); Serial.print("  "); Serial.print(err_c); Serial.print(" "); Serial.print(err_o);
 			Serial.print(" Y: "); Serial.print(mympu.ypr[0]);
@@ -148,15 +173,21 @@ void loop() {
 			Serial.print("\tgy: "); Serial.print(mympu.gyro[0]);
 			Serial.print(" gp: "); Serial.print(mympu.gyro[1]);
 			Serial.print(" gr: "); Serial.println(mympu.gyro[2]);
-		}
-	}
+		}*/
 
-  else {
-    delay(250);
-    digitalWrite(ledPin, HIGH);
-    delay(250);
-    digitalWrite(ledPin, LOW);
-  }
+    if (!(c%25)) {
+      Serial.print(np);// Serial.print("  "); Serial.print(err_c); Serial.print(" "); Serial.print(err_o);
+      Serial.print("  "); 
+      Serial.print(mympu.ypr[0]); //yaw
+      Serial.print("  "); 
+      Serial.print(mympu.ypr[1]); //pitch
+      Serial.print("  "); 
+      Serial.println(mympu.ypr[2]); //roll
+      //Serial.print("\tgy: "); Serial.print(mympu.gyro[0]);
+      //Serial.print(" gp: "); Serial.print(mympu.gyro[1]);
+      //Serial.print(" gr: "); Serial.println(mympu.gyro[2]);
+    }
+	}
 }
 /*
 void ticker(){
